4bbaaf0304cdf3bbcc26818d73cbbc279dfc10f2
naormalihi <naor.malihi@gmail.com>
2026-05-10 08:36:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
-const CACHE = 'rinat-asher-v2026.05.10.0515';
+const CACHE = 'rinat-asher-v2026.05.10.0529';
 
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-     .then(() => self.clients.matchAll())
-     .then(clients => clients.forEach(c => c.postMessage('reload')))
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.postMessage('reload')))
   );
 });
 
 self.addEventListener('fetch', e => {
-  // תמיד נסה לטעון מהרשת קודם
   e.respondWith(
     fetch(e.request, {cache: 'no-cache'})
       .then(res => {
